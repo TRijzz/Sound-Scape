754af2dc40fd373c90cde941192cb5e054fbea9e
TRIjan <trijanratnashakya@gmail.com>
2025-10-05 14:38:18 +0545

    new changes made for db

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
-import { connectDB } from './config/db.js';
+import { connectDB } from './src/config/db.js';
 import authRoutes from './routes/auth.routes.js';
 import userRoutes from './routes/user.routes.js';
 import artistRoutes from './routes/artist.routes.js';
